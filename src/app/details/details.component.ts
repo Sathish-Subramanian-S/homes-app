59734e99b16fb560a77c4b202d96f0f09628378c
Sathish-Subramanian-S <sathishit157@gmail.com>
2026-02-20 13:50:48 -0500

    Added route

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, RouterModule } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule, RouterModule],
   template: `
-    <p>
-      details works!
-    </p>
+    <p> Detail  Works  {{housingLocation?.id}}</p>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService : HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+
+  constructor(){
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
 
 }
